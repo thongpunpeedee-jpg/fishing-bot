b497a4ff4d4f6648db4a736ac790fc46762dff59
thongpunpeedee-jpg <thongpunpeedee@gmail.com>
2026-03-19 19:37:58 +0700

    Update auto_fisher.py

--- auto_fisher.py
+++ auto_fisher.py
@@ -136,7 +136,7 @@ class AutoDetectionWorker(QObject):
 class DetectionDisplay(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("🎣AUTO🎣 (test1)")
+        self.setWindowTitle("🎣AUTO🎣")
         self.setFixedSize(600, 150)
         self.setWindowFlags(Qt.WindowType.WindowStaysOnTopHint)
         self.setStyleSheet("background-color: #000;")
